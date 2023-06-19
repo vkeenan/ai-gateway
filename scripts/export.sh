7b1b3384c946f3321d92743716e6dd683b6a0e66
Vernon Keenan <vkeenan@gmail.com>
2023-06-19 21:12:12 +0000

    new promps

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,7 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Variable Declarations
 source $DIR/.env
 
-sfdx data export tree  --json --output-dir ./data --query \
+sfdx data export tree  --json --output-dir ./data \
+    --target-org $ORG_ALIAS \
+    --query \
         "select id, name, model__c, order__c, \
     parameters__c, UsedCount__c, \
     tags__c, prompt__c, system__c, title__c, maxtokens__c, \
